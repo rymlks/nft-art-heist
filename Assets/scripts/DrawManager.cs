b74d440b1d2503414e5f36f35515f2ca74b67158
commodare <33709448+commodare@users.noreply.github.com>
2022-02-23 11:11:37 -0500

    Implemented drawing color on mousedown

--- Assets/scripts/DrawManager.cs
+++ Assets/scripts/DrawManager.cs
@@ -14,7 +14,7 @@ public class DrawManager : MonoBehaviour
     {
         myImage = GetComponent<Image>();
 
-        drawing = new Texture2D(16, 16);    
+        drawing = new Texture2D(32, 32);  
         drawing.filterMode = FilterMode.Point;
         Sprite mySprite = Sprite.Create(drawing, new Rect(0.0f, 0.0f, drawing.width, drawing.height), Vector2.one);
         myImage.sprite = mySprite;
@@ -22,19 +22,46 @@ public class DrawManager : MonoBehaviour
         myImage.color = Color.white;
 
         Debug.Log("It did the thing!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Vector3 mousePos = Input.mousePosition;
 
+        Vector3[] corners = new Vector3[4];
+        GetComponent<Image>().rectTransform.GetWorldCorners(corners);
+        Rect newRect = new Rect(corners[0], corners[2]-corners[0]);
+        //Debug.Log(newRect.Contains(Input.mousePosition));
+
+        Vector3 translatedMousePos = mousePos - corners[0];
+        
+        int width = drawing.width;
+
+        int height = drawing.height;
+
+        translatedMousePos.x = Mathf.Floor(width * translatedMousePos.x/newRect.width);
+        translatedMousePos.y =  Mathf.Floor(height * translatedMousePos.y/newRect.height);
+
+        Debug.Log(translatedMousePos);
+
+        bool widthValid = translatedMousePos.x >=0 && translatedMousePos.x < drawing.width;
+        bool heightValid = translatedMousePos.y >=0 && translatedMousePos.y < drawing.height;
+        bool isMouseDown = Input.GetMouseButton(0);
+
+        if (widthValid && heightValid && isMouseDown) {
+
+            drawing.SetPixel((int) translatedMousePos.x, (int) translatedMousePos.y, Color.black);
+            drawing.Apply();
+
+        }
 
     }
 
     private void OnMouseDrag()
     {
-        
     }
 
 }
